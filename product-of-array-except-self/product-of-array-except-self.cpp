f49681fcc5f4da70895a71f877a549f8469ea889
ChrisAshton84 <ChrisAshton84@gmail.com>
2020-12-09 11:18:09 -0600

    working commit - Created using LeetHub

---
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int product = 1;
+        vector<int> result(nums.size(), 1);
+        for (int i = 0; i < nums.size(); i++) {
+            result[i] = product;
+            product *= nums[i];
+        }
+        
+        product = 1;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            result[i] *= product;
+            product *= nums[i];
+        }
+        
+        return result;
+    }
+};
